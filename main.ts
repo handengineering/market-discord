e1066c162d8109deea753556e1801ad55789a2e3
Lewis Flude <lewis@lewisflude.com>
2022-10-14 22:45:27 +0100

    Fix app

--- main.ts
+++ main.ts
@@ -3,18 +3,6 @@ import { serve } from "https://deno.land/std@0.159.0/http/server.ts";
 import { parseURL, connect } from "https://deno.land/x/redis/mod.ts";
 import { fetchAllGuildMembers } from "./fetchAllGuildMembers.ts";
 
-const port = 8081;
-
-const handler = (request: Request): Response => {
-  const body = `Your user-agent is:\n\n${
-    request.headers.get("user-agent") ?? "Unknown"
-  }`;
-
-  return new Response(body, { status: 200 });
-};
-
-await serve(handler, { port });
-
 const redisUrl = config().REDIS_URL;
 const connectionOptions = parseURL(redisUrl);
 
@@ -26,3 +14,15 @@ setInterval(async () => {
   console.log("GETTING");
   await redis.set("discordGuildMembers", allGuildMembers);
 }, 5000);
+
+const port = 8081;
+
+const handler = (request: Request): Response => {
+  const body = `Your user-agent is:\n\n${
+    request.headers.get("user-agent") ?? "Unknown"
+  }`;
+
+  return new Response(body, { status: 200 });
+};
+
+await serve(handler, { port });
